6cf1716804480a4f8129d2efeeef993fad4a394a
rainui28 <rainui.ly@tietoevry.com>
2024-01-02 12:46:55 +0100

    new: adding /pr command

---
+++ README.md
@@ -0,0 +1,19 @@
+
+## Google form
+
+https://docs.google.com/forms/u/0/d/e/1FAIpQLSeEomm3cNsaDCmhAfWVHP8dqGP1jGwo_MFcnEsEj2tRyLHcyQ/formResponse
+
+Request Content-Type: `application/x-www-form-urlencoded`
+
+```
+entry.742237483=48405377&entry.2038241630=12&entry.1321964420=50&entry.620889826=100&entry.1205398535=160&entry.1509725706=-80&dlut=1704191638643&entry.1509725706_sentinel=&fvv=1&partialResponse=%5Bnull%2Cnull%2C%225667717789054114695%22%5D&pageHistory=0&fbzx=5667717789054114695
+```
+
+```
+entry.742237483	"48405377"
+entry.2038241630	"12"
+entry.1321964420	"50"
+entry.620889826	"100"
+entry.1205398535	"160"
+entry.1509725706	"-80"
+```
---
+++ server.py
@@ -0,0 +1,43 @@
+import logging
+import json
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.constants import ParseMode
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+with open("bot.json") as properties_fp:
+    BOT_PROPERTIES = json.load(properties_fp)["properties"]
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+    logging.info("/pr has been triggered")
+
+
+async def pr(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_to_send = (
+        f"*Entrer vos PRs [via ce formulaire]({BOT_PROPERTIES['GOOGLE_FORM_PR']})*"
+    )
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text_to_send,
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
+    logging.info("/pr has been triggered")
+
+
+if __name__ == "__main__":
+    with open("token") as token_fp:
+        TOKEN_BOT = token_fp.read()
+        application = ApplicationBuilder().token(TOKEN_BOT).build()
+        start_handler = CommandHandler("start", start)
+        pr_handler = CommandHandler("pr", pr)
+        application.add_handler(start_handler)
+        application.add_handler(pr_handler)
+
+        application.run_polling()
