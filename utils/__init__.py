59ce54b13804a7bd9f003df69e7e9c42d944b1cb
rainui28 <rainui.ly@tietoevry.com>
2024-03-28 22:33:50 +0100

    new: multi-chats for schedulers

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,4 @@
-from jobs import get_job_by_name
 from .meta import Singleton
 from .manager import JobManager, CommandManager
 from .parser import PropertyParser
 from .botconfig import BotConfig
-
