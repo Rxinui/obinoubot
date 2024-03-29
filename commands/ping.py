2a8068298a9f6e29b0e2f1562626576b2642467b
rainui28 <rainui.ly@tietoevry.com>
2024-03-29 19:07:43 +0100

    fix: huge fix on method is_user_member_of_chat
    - fix: now make sure that a user is a member
    of a given chat

--- commands/ping.py
+++ commands/ping.py
@@ -1,12 +1,8 @@
-from telegram import ChatMember, Update
-from telegram.constants import ParseMode
+import logging
+from telegram import Update
 from telegram.ext import ContextTypes
 from commands.base import BaseMessageCommand
-from exceptions.errors import CommandNotAllowedError
-from services.bot import BotMessageService
 from utils.botconfig import BotConfig
-from typing import Self
-from telegram.error import BadRequest
 
 
 class PingCommand(BaseMessageCommand):
@@ -17,6 +13,4 @@ class PingCommand(BaseMessageCommand):
     async def execute(
         self, update: Update, context: ContextTypes.DEFAULT_TYPE, **kwargs
     ):
-        everyday_id = 5414239606
-        service = BotMessageService(self._botconfig, update, context)
-        await service.notify_authorized_chats_where_user_is_member(everyday_id, "OK")
+        logging.warning("PONG")
